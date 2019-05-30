a8e82f4a05b291b1ed57394737fb368a2b040e95
Белов Александр Игоревич <7alexcreeper7@gmail.com>
2019-05-30 21:15:39 +0300

    мама я прогер

--- castleFlex_alfa/MainWindow.xaml.cs
+++ castleFlex_alfa/MainWindow.xaml.cs
@@ -16,6 +16,7 @@ using System.Data.SQLite;
 using System.Data;
 using System.Data.Entity;
 using System.IO;
+//using System.Drawing;
 
 namespace castleFlex_alfa
 {
@@ -32,10 +33,46 @@ namespace castleFlex_alfa
             db = new ApplicationContext();
             db.cards.Load();
             this.DataContext = db.cards.Local.ToBindingList();
-            //MessageBox.Show(db.cards.Find(3).name);
-            //картинка не работает :с
-            //testImg.Source = db.cards.Find(1).pic;
+            MessageBox.Show(db.cards.Find(3).name);
+            //картинка не работает: с
+            //testImg.Source = (db.cards.Find(1).pic);
+            //работает если всё нормально преобразовать!
+            testImg.Source = CreateImage(db.cards.Find(9).pic);
+
+            // хороший код, но хуйня жи столько строчек не выносить в другую функцю
+            //MemoryStream byteStream = new MemoryStream(db.cards.Find(1).pic);
+            //BitmapImage image = new BitmapImage();
+            //image.BeginInit();
+            //image.StreamSource = byteStream;
+            //image.EndInit();
+            //testImg.Source = image;
         }
+
+        //public static byte[] imageToByteArray(System.Drawing.Image imageIn)
+        //{
+        //    MemoryStream ms = new MemoryStream();
+        //    imageIn.Save(ms, System.Drawing.Imaging.ImageFormat.Gif);
+        //    return ms.ToArray();
+        //}
+        //public static System.Drawing.Image byteArrayToImage(byte[] byteArrayIn)
+        //{
+        //    MemoryStream ms = new MemoryStream(byteArrayIn);
+        //    System.Drawing.Image returnImage = System.Drawing.Image.FromStream(ms);
+        //    return returnImage;
+        //} //говнокод для вин форм и System.Drawing, т.к. мы используем WPF - юзелес
+
+
+
+        public static System.Windows.Media.Imaging.BitmapImage CreateImage(byte[] imageData)
+        {
+            MemoryStream byteStream = new MemoryStream(imageData);
+            BitmapImage image = new BitmapImage();
+            image.BeginInit();
+            image.StreamSource = byteStream;
+            image.EndInit();
+            return image;
+        }  // функция преобразование byto to ImageSourse
+
 
         private void FromStream(MemoryStream mStream)
         {
