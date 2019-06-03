e559b1bd4809c52b1f0fe10d09cabea89419454d
Albert S <lw204@13.a1p.me>
2019-06-03 08:03:08 +0300

    UI modified

---
+++ castleFlex_alfa/net.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace castleFlex_alfa
+{
+    class net
+    {
+        class Client
+        {
+            public void Connection(string ip,int port)
+            {
+                TcpClient Connection = new TcpClient(ip, port);
+            }
+        }
+        class Server
+        {
+
+        }
+    }
+}
