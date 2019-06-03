1457658bbb72b7e194a12bc11046e0b26fae3e66
Белов Александр Игоревич <7alexcreeper7@gmail.com>
2019-06-03 22:45:23 +0300

    е6ашимкакмаги

--- castleFlex_alfa/MainWindow.xaml.cs
+++ castleFlex_alfa/MainWindow.xaml.cs
@@ -34,6 +34,8 @@ namespace castleFlex_alfa
             image.EndInit();
             return image;
         }  // функция преобразование byto to ImageSourse
+        //OneGameWin onegame = new OneGameWin();
+        //TwoGameWin toggame = new TwoGameWin();
         public MainWindow()
         {
             InitializeComponent();
@@ -42,48 +44,19 @@ namespace castleFlex_alfa
             db = new ApplicationContext();
             db.cards.Load();
             this.DataContext = db.cards.Local.ToBindingList();
-            //MessageBox.Show(db.cards.Find(3).name);
-            //картинка не работает: с
-            //testImg.Source = (db.cards.Find(1).pic);
-            //работает если всё нормально преобразовать!
-            //testImg.Source = CreateImage(db.cards.Find(9).pic);
-
-            // хороший код, но хуйня жи столько строчек не выносить в другую функцю
-            //MemoryStream byteStream = new MemoryStream(db.cards.Find(1).pic);
-            //BitmapImage image = new BitmapImage();
-            //image.BeginInit();
-            //image.StreamSource = byteStream;
-            //image.EndInit();
-            //testImg.Source = image;
-
-            
         }
-
-
-        //public static byte[] imageToByteArray(System.Drawing.Image imageIn)
-        //{
-        //    MemoryStream ms = new MemoryStream();
-        //    imageIn.Save(ms, System.Drawing.Imaging.ImageFormat.Gif);
-        //    return ms.ToArray();
-        //}
-        //public static System.Drawing.Image byteArrayToImage(byte[] byteArrayIn)
-        //{
-        //    MemoryStream ms = new MemoryStream(byteArrayIn);
-        //    System.Drawing.Image returnImage = System.Drawing.Image.FromStream(ms);
-        //    return returnImage;
-        //} //говнокод для вин форм и System.Drawing, т.к. мы используем WPF - юзелес
-        
         private void OneGame(object sender, RoutedEventArgs e) 
         {
             OneGameWin onegame = new OneGameWin();
             onegame.Show();
+            //onegame.InitializeComponent();
         }
         private void TwoGether(object sender, RoutedEventArgs e)
         {
             TwoGameWin toggame = new TwoGameWin();
             toggame.Show();
+            //toggame.InitializeComponent();
         }
-
         private void MediaElement_MediaEnded_1(object sender, RoutedEventArgs e)
         {
             TimeSpan ts = new TimeSpan(0, 0, 0, 0, 0);
@@ -94,7 +67,6 @@ namespace castleFlex_alfa
         {
             Application.Current.Shutdown();
         }
-
         private void ExitButton_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
--- castleFlex_alfa/OneGameWin.xaml.cs
+++ castleFlex_alfa/OneGameWin.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Data.Entity;
 using System.IO;
+using System.Reflection;
 
 namespace castleFlex_alfa
 {
@@ -23,6 +24,8 @@ namespace castleFlex_alfa
     public partial class OneGameWin : Window
     {
         ApplicationContext db;
+        public int k;
+        public int[] arr = new int[100];
         public cardList cards = new cardList();
         public class player
         {
@@ -53,27 +56,87 @@ namespace castleFlex_alfa
         }
         public static player p1 = new player(50, 0, 1, 15, 1, 15, 1, 15);
         public static player p2 = new player(50, 0, 1, 15, 1, 15, 1, 15);
+        public void updateInfo(player a, player b)
+        {
+            playerMages.Content = $"{a.wiz}";
+            playerMagic.Content = $"{a.magic}";
+            playerRecruts.Content = $"{a.rec}";
+            playerArmy.Content = $"{a.army}";
+            playerMines.Content = $"{a.mine}";
+            playerOre.Content = $"{a.ore}";
+            ptv.Content = a.tower;
+            pwv.Content = a.wall;
+
+            enemyMages.Content = $"{b.wiz}";
+            enemyMagic.Content = $"{b.magic}";
+            enemyRecruts.Content = $"{b.rec}";
+            enemyArmy.Content = $"{b.army}";
+            enemyMines.Content = $"{b.mine}";
+            enemyOre.Content = $"{b.ore}";
+            etv.Content = b.tower;
+            ewv.Content = b.wall;
+            //t3z.Height = 125;
+            //System.Windows.Thickness bs = new System.Windows.Thickness(85, 125, 0, 0);
+            //t3.Margin = bs;
+        }
+        public void krupbe()
+        {
+
+        }
         public OneGameWin()
         {
             InitializeComponent();
             db = new ApplicationContext();
             db.cards.Load();
-            //this.DataContext = db.cards.Local.ToBindingList();
-            Card1.Source = CreateImage(db.cards.Find(9).pic);
-            Card2.Source = CreateImage(db.cards.Find(11).pic);
-            Card3.Source = CreateImage(db.cards.Find(13).pic);
-            Card4.Source = CreateImage(db.cards.Find(5).pic);
-            Card5.Source = CreateImage(db.cards.Find(7).pic);
-            Card6.Source = CreateImage(db.cards.Find(39).pic);
+            //player ppp = new player(50, 0, 1, 15, 1, 15, 1, 15);
+            //p1 = ppp;
+            //p2 = ppp;
+
+        var rand = new Random();
+            var knownNumbers = new HashSet<int>();
+            for (int i = 0; i < arr.Length; i++) // работает - не трогай!
+            {
+                int newElement;
+                do
+                {
+                    newElement = rand.Next(100) + 1;
+                } while (!knownNumbers.Add(newElement));
+                arr[i] = newElement;
+            }
+
+            for (int i = 0; i <= 5; i++)
+            {
+                p1.hand[i] = arr[i];
+                p1.usedCards.Add(p1.hand[i]);
+            }
+            Card1.Source = CreateImage(db.cards.Find(p1.hand[0]).pic);
+            Card2.Source = CreateImage(db.cards.Find(p1.hand[1]).pic);
+            Card3.Source = CreateImage(db.cards.Find(p1.hand[2]).pic);
+            Card4.Source = CreateImage(db.cards.Find(p1.hand[3]).pic);
+            Card5.Source = CreateImage(db.cards.Find(p1.hand[4]).pic);
+            Card6.Source = CreateImage(db.cards.Find(p1.hand[5]).pic);
+            for (int i = 0; i <= 5; i++)
+            {
+                p2.hand[i] = arr[i];
+                p2.usedCards.Add(p2.hand[i]);
+            }
+            k = 12;
+
+            
+            updateInfo(p1, p2);
+            //Card1.Source = CreateImage(db.cards.Find(9).pic);
+            //Card2.Source = CreateImage(db.cards.Find(11).pic);
+            //Card3.Source = CreateImage(db.cards.Find(13).pic);
+            //Card4.Source = CreateImage(db.cards.Find(5).pic);
+            //Card5.Source = CreateImage(db.cards.Find(7).pic);
+            //Card6.Source = CreateImage(db.cards.Find(39).pic);
             this.DataContext = db.cards.Local.ToBindingList();
-            //t3z.Height = 125;
-            //System.Windows.Thickness bs = new System.Windows.Thickness(85, 125, 0, 0);
-            //t3.Margin = bs;
+            
         }
 
         public void resMessage(int cost)
         {
-            MessageBox.Show("Недостаточно ресурсов");
+            MessageBox.Show("Недостаточно ресурсов, карта сбрасывается");
         }
 
         public void startGame()
@@ -98,11 +161,194 @@ namespace castleFlex_alfa
         //    MediaElement.Position = ts;
         //    MediaElement.Play();
         //} //зацикливание фона
-        
+        public void endTurn()
+        {
+            if ((p2.tower <= 0) || (p1.tower >= 100))
+            {
+                MessageBox.Show("Вы победили!", "Игра окончена", MessageBoxButton.OK, MessageBoxImage.Information);
+                Close();
+            }
+            else if ((p1.tower <= 0) || (p2.tower >= 100))
+            {
+                MessageBox.Show("Вы проиграли!", "Игра окончена", MessageBoxButton.OK, MessageBoxImage.Information);
+                Close();
+            }
+            newTurn();
+        }
+        public void newTurn()
+        {
+            if (p1.wiz <= 0) { p1.wiz = 1; }
+            if (p1.rec <= 0) { p1.rec = 1; }
+            if (p1.mine <= 0) { p1.mine = 1; }
+
+            if (p2.wiz <= 0) { p2.wiz = 1; }
+            if (p2.rec <= 0) { p2.rec = 1; }
+            if (p2.mine <= 0) { p2.mine = 1; }
+
+            if (p1.magic < 0) { p1.magic = 0; }
+            if (p1.army < 0) { p1.army = 0; }
+            if (p1.ore < 0) { p1.ore = 0; }
+
+            if (p2.magic < 0) { p2.magic = 0; }
+            if (p2.army < 0) { p2.army = 0; }
+            if (p2.ore < 0) { p2.ore = 0; }
+
+            if (Card1.IsEnabled == false)
+            {
+                do
+                {
+                    p1.hand[0] = arr[k];
+                    if (p1.usedCards.Count == 99)
+                    {
+                        MessageBox.Show("В колоде закончились карты!\nРаздаётся новая...", "Колода пуста!");
+                        p1.usedCards.Clear();
+                        break;
+                    }
+                } while (p1.usedCards.Contains(k));
+                p1.usedCards.Add(p1.hand[0]);
+                Card1.IsEnabled = true;
+                k++;
+            }
+            if (Card2.IsEnabled == false)
+            {
+                do
+                {
+                    p1.hand[1] = arr[k];
+                    if (p1.usedCards.Count == 99)
+                    {
+                        MessageBox.Show("В колоде закончились карты!\nРаздаётся новая...", "Колода пуста!");
+                        p1.usedCards.Clear();
+                        break;
+                    }
+                } while (p1.usedCards.Contains(k));
+                p1.usedCards.Add(p1.hand[1]);
+                Card2.IsEnabled = true;
+                k++;
+            }
+            if (Card3.IsEnabled == false)
+            {
+                do
+                {
+                    p1.hand[2] = arr[k];
+                    if (p1.usedCards.Count == 99)
+                    {
+                        MessageBox.Show("В колоде закончились карты!\nРаздаётся новая...", "Колода пуста!");
+                        p1.usedCards.Clear();
+                        break;
+                    }
+                } while (p1.usedCards.Contains(k));
+                p1.usedCards.Add(p1.hand[2]);
+                Card3.IsEnabled = true;
+                k++;
+            }
+            if (Card4.IsEnabled == false)
+            {
+                do
+                {
+                    p1.hand[3] = arr[k];
+                    if (p1.usedCards.Count == 99)
+                    {
+                        MessageBox.Show("В колоде закончились карты!\nРаздаётся новая...", "Колода пуста!");
+                        p1.usedCards.Clear();
+                        break;
+                    }
+                } while (p1.usedCards.Contains(k));
+                p1.usedCards.Add(p1.hand[3]);
+                Card4.IsEnabled = true;
+                k++;
+            }
+            if (Card5.IsEnabled == false)
+            {
+                do
+                {
+                    p1.hand[4] = arr[k];
+                    if (p1.usedCards.Count == 99)
+                    {
+                        MessageBox.Show("В колоде закончились карты!\nРаздаётся новая...", "Колода пуста!");
+                        p1.usedCards.Clear();
+                        break;
+                    }
+                } while (p1.usedCards.Contains(k));
+                p1.usedCards.Add(p1.hand[4]);
+                Card5.IsEnabled = true;
+                k++;
+            }
+            if (Card6.IsEnabled == false)
+            {
+                do
+                {
+                    p1.hand[5] = arr[k];
+                    if (p1.usedCards.Count == 99)
+                    {
+                        MessageBox.Show("В колоде закончились карты!\nРаздаётся новая...", "Колода пуста!");
+                        p1.usedCards.Clear();
+                        break;
+                    }
+                } while (p1.usedCards.Contains(k));
+                p1.usedCards.Add(p1.hand[5]);
+                Card6.IsEnabled = true;
+                k++;
+            }
+            Card1.Source = CreateImage(db.cards.Find(p1.hand[0]).pic);
+            Card2.Source = CreateImage(db.cards.Find(p1.hand[1]).pic);
+            Card3.Source = CreateImage(db.cards.Find(p1.hand[2]).pic);
+            Card4.Source = CreateImage(db.cards.Find(p1.hand[3]).pic);
+            Card5.Source = CreateImage(db.cards.Find(p1.hand[4]).pic);
+            Card6.Source = CreateImage(db.cards.Find(p1.hand[5]).pic);
+            p1.magic += p1.wiz;
+            p1.army += p1.rec;
+            p1.ore += p1.mine;
+            updateInfo(p1, p2);
+        }
+
         private void MediaElement_MouseDown(object sender, MouseButtonEventArgs e)
         {
             if (e.ChangedButton == MouseButton.Left)
                 this.DragMove();
+        }
+
+        private void Card1_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            cardList.id = p1.hand[0];
+            MethodInfo card = cards.GetType().GetMethod(db.cards.Find(p1.hand[0]).name);
+            card.Invoke(this, null);
+            updateInfo(p1, p2);
+            if (db.cards.Find(p1.hand[0]).doubleTurn == 0)
+            {
+                Card1.IsEnabled = false;
+                endTurn();
+            } else Card1.IsEnabled = false;
+        }
+        private void Card2_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+        private void Card3_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("иди нахуй");
+        }
+        private void Card4_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+        private void Card5_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+        private void Card6_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void menu_Click(object sender, RoutedEventArgs e)
+        {
+            this.IsEnabled = false;
+            if (MessageBox.Show("Вы действительно хотите выйти из матча?", "Выход", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+            {
+                p1.usedCards.Clear();
+                this.Close();
+            }
+            this.IsEnabled = true;
         }
     }
 }
